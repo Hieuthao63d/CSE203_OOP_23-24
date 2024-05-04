6ee3facc81416bff35f759b7a1d6e8fca2c4d41f
Hieuthao63d <125064888+Hieuthao63d@users.noreply.github.com>
2024-05-04 08:59:29 +0700

    v2

--- Lab2/Lab2/src/main/java/com/mycompany/lab2/Lab2.java
+++ Lab2/Lab2/src/main/java/com/mycompany/lab2/Lab2.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.lab2;
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,29 @@ package com.mycompany.lab2;
 public class Lab2 {
 
     public static void main(String[] args) {
+        //Q1
+        Scanner scanner = new Scanner(System.in);
+
+        Q1_Complex c1 = new Q1_Complex();
+        System.out.println("Enter first complex number:");
+        c1.input(scanner);
+
+        Q1_Complex c2 = new Q1_Complex();
+        System.out.println("Enter second complex number:");
+        c2.input(scanner);
+
+        Q1_Complex sum = c1.plus(c2);
+        System.out.print("Sum: ");
+        sum.print();
+
+        Q1_Complex difference = c1.minus(c2);
+        System.out.print("Difference: ");
+        difference.print();
+
+        Q1_Complex product = c1.multiply(c2);
+        System.out.print("Product: ");
+        product.print();
+        
         
     }
 }
