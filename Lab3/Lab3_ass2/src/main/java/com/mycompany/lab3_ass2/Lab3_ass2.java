066a278744121044015c86c0e946e96f77172b36
Hieuthao63d <125064888+Hieuthao63d@users.noreply.github.com>
2024-05-11 11:18:15 +0700

    ass2

---
+++ Lab3/Lab3_ass2/src/main/java/com/mycompany/lab3_ass2/Lab3_ass2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab3_ass2;
+
+/**
+ *
+ * @author Phan Thao
+ */
+public class Lab3_ass2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
