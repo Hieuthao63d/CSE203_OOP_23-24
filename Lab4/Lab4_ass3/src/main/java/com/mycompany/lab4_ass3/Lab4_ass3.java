5d238decdf4f42410dd702f08e903084d110bb5c
Hieuthao63d <125064888+Hieuthao63d@users.noreply.github.com>
2024-05-18 10:11:09 +0700

    Lab4_ass2-3

---
+++ Lab4/Lab4_ass3/src/main/java/com/mycompany/lab4_ass3/Lab4_ass3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab4_ass3;
+
+/**
+ *
+ * @author Phan Thao
+ */
+public class Lab4_ass3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
