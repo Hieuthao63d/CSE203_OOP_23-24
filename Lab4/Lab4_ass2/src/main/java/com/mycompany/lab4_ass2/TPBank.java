3a1342d95b3225b52d7e546ba9d1603676dfb713
Hieuthao63d <125064888+Hieuthao63d@users.noreply.github.com>
2024-05-18 10:57:28 +0700

    v2

---
+++ Lab4/Lab4_ass2/src/main/java/com/mycompany/lab4_ass2/TPBank.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab4_ass2;
+
+/**
+ *
+ * @author Phan Thao
+ */
+public class TPBank implements Bank{
+    @Override
+    public String getBankName(){
+        publ
+    }
+}
